b3855dc72b74c3dfb6545bf75a0123d11d11ca4b
alogumm <54426896+alogumm@users.noreply.github.com>
2019-10-11 08:52:05 -0500

    Las tareas 1 y 2 se han agregado

---
+++ TAREAS/2/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main( int argc, char *argv[] ){
+	
+	int num;
+	
+	printf("%s \n", argv[1]);
+	num=atoi(argv[1]);
+	
+    if (num%2==0)
+        printf("1 \n");
+      else
+       printf("0 \n");
+       
+       return 0;
+}
