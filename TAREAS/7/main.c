7ca72f26e58f14153591bc021ab672cd6f64eb1d
alogumm <54426896+alogumm@users.noreply.github.com>
2019-11-05 19:35:08 -0600

    Se ha agregado la tarea 2

---
+++ TAREAS/7/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char*argv[]){
+	//se declaran las variables
+	int a;
+	int b;
+	int c=1;
+	a=atof(argv[1]);
+	b=atof(argv[2]);
+	//decimos en donde lo guardamos
+	for (int i=0;i<b;i++){
+		c=c*a;
+		//se va a guardar e ir multiplicando
+	}
+	printf("%.2d\n", c); //imprime el resultado
+	return 0;
+	
+}
